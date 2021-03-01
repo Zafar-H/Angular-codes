b86ca6cc50dc15ed1909bafca935e415e13beb96
Zafar-H <76478818+Zafar-H@users.noreply.github.com>
2021-03-01 15:03:39 +0530

    Add files via upload

---
+++ angular-httpClient/app/app.module.ts
@@ -0,0 +1,91 @@
+import { HttpClientModule } from '@angular/common/http';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CoursesService } from './courses.service';
+import { SummaryPipe } from './summary.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
+import { PanelComponent } from './panel/panel.component';
+import { LikeComponent } from './like/like.component';
+import { InputFormatDirective } from './input-format.directive';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { UdemyInstructorComponent } from './udemy-instructor/udemy-instructor.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostService } from './services/post.service';
+import { AppErrorHandler } from './common/app-error-handler';
+import { GithubFollowersComponent } from './github-followers/github-followers.component';
+import { GithubFollowersService } from './github-followers.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    SummaryPipe,
+    FavoriteComponent,
+    TitleCasePipe,
+    PanelComponent,
+    LikeComponent,
+    InputFormatDirective,
+    ContactFormComponent,
+    UdemyInstructorComponent,
+    SignupFormComponent,
+    NewCourseFormComponent,
+    ChangePasswordComponent,
+    PostsComponent,
+    GithubFollowersComponent,
+    NavbarComponent,
+    HomeComponent,
+    GithubProfileComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '', 
+        component: HomeComponent 
+      },
+      {
+        path: 'followers/:id/:username', 
+        component: GithubProfileComponent 
+      },
+      {
+        path: 'followers', 
+        component: GithubFollowersComponent 
+      },
+      {
+        path: 'posts', 
+        component: PostsComponent 
+      },
+      {
+        path: '**', 
+        component: NotFoundComponent 
+      }
+    ]) 
+  ],
+  providers: [
+    CoursesService,
+    PostService,
+    GithubFollowersService,
+    {provide: ErrorHandler, useClass: AppErrorHandler}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
