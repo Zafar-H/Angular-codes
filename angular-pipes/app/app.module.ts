c3a516fd763b3c1dfc8f43c9b49aa5959e7291c8
Zafar-H <76478818+Zafar-H@users.noreply.github.com>
2021-03-01 15:00:22 +0530

    Add files via upload

---
+++ angular-pipes/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PlayersListComponent } from './players-list/players-list.component';
+import { PlayerTitlePipe } from './players-list/playerTitle.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlayersListComponent,
+    PlayerTitlePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
